5776a52ad2cf6c41cfce7011d911f58a395a7ae1
Nikita Zapadnov <niza93@yandex.ru>
2024-11-07 12:48:47 +0300

    Files added to git

---
+++ MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiStringFormatIssue.ViewModels
+{
+  public class MainViewModel : INotifyPropertyChanged
+  {
+    private float mFloatValue;
+
+    public float FloatValue
+    {
+      get => mFloatValue;
+      set => Set(ref mFloatValue, value);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+      if (!Equals(field, value))
+      {
+        field = value;
+        RaisePropertyChanged(propertyName);
+        return true;
+      }
+      return false;
+    }
+  }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using BarcodeScanning;
+using MauiStringFormatIssue.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace MauiStringFormatIssue
+{
+  public static class MauiProgram
+  {
+    public static MauiApp CreateMauiApp()
+    {
+      var builder = MauiApp.CreateBuilder();
+      builder
+          .UseMauiApp<App>()
+          .UseBarcodeScanning()
+          .ConfigureFonts(fonts =>
+          {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+          });
+      builder.Services.AddSingleton<MainViewModel>();
+      builder.Services.AddSingleton<MainPage>();
+#if DEBUG
+      builder.Logging.AddDebug();
+#endif
+
+      return builder.Build();
+    }
+  }
+}
